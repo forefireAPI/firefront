834ab589eaaa94d950407de00cf2eada5f96aa8c
Jean-Baptiste Filippi <filippi@univ-corse.fr>
2015-06-25 14:50:00 +0200

    updated AT script for good copy

--- tools/NCfile.py
+++ tools/NCfile.py
@@ -5,68 +5,45 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import matplotlib.cbook as cbook
 
+#
+# The purpose of this script is to copy a AT map to another one and impose some data (here a png file)
+# It may be used as an example to look at how to write AT map for any purposes 
+#
+
 fname = "/Users/filippi/workspace/fireflux2/ffcase/ForeFire/ForeFire.0.nc"
 fnameout = "/Users/filippi/workspace/fireflux2/ffcase/ForeFire/bmapout.nc"
 
 nc = netcdf.netcdf_file(fname, 'r')
 nco = netcdf.netcdf_file(fnameout, 'w')
 
-print nc.variables
-print nc.dimensions 
 
 for dim in nc.dimensions:
-    print dim
     nco.createDimension(dim,nc.dimensions[dim] )
+for key in nc.variables.keys():
+    print "copying" , key, nc.variables[key].dimensions
+    nco.createVariable(key, nc.variables[key][:].dtype, nc.variables[key].dimensions)
+    nco.variables[key][:] = np.array(nc.variables[key][:])
+    for attvar in nc.variables[key]._attributes:
+        print "   copying attribute " , key+ "."+attvar ," = ", nc.variables[key].__dict__[attvar]
+        nco.variables[key]._attributes[attvar] = nc.variables[key]._attributes[attvar]
 
-domain = {}
-domain["SWx"] = -10. 
-domain["SWy"] = -10. 
-domain["Lx"] = 1020. 
-domain["Ly"] = 2020. 
-domain["Lz"] = 0 
-domain["refYear"] = 2013 
-domain["refDay"] = 30 
 
-nco.createVariable("arrival_time_of_front", nc.variables["arrival_time_of_front"][:].dtype, nc.variables["arrival_time_of_front"].dimensions)
-nco.createVariable("cell_active", nc.variables["cell_active"][:].dtype, nc.variables["cell_active"].dimensions)
-nco.createVariable("domain", nc.variables["domain"][:].dtype, nc.variables["domain"].dimensions)
+nc.close()
 
-toto = nc.variables['arrival_time_of_front']
-ca = nc.variables['cell_active']
 ca2 = nco.variables['cell_active']
-
-do2 = nco.variables['domain']
-do2.SWx = -10. 
-do2.SWy = -10. 
-do2.Lx = 1020. 
-do2.Ly = 2020. 
-do2.Lz = 0 
-do2.refYear = 2013 
-do2.refDay = 30 
-
-
-
 ca2[:].fill(1)
 
 image_file = cbook.get_sample_data('/Users/filippi/workspace/fireflux2/ffcase/bmapForced.png')
 image = plt.imread(image_file)
 values = (image[::-1,:,2] * (8 * 60) ) + 75600
-
-v2 = np.where(values == np.max(values),-9999, values)
- 
-
-toto2 = nco.variables['arrival_time_of_front']
-toto2[:] = v2
+destAT = nco.variables['arrival_time_of_front']
+destAT[:] = np.where(values == np.max(values),-9999, values)
 
 nco.close()
 
-print np.max(values), np.min(values), np.shape(values), np.shape(toto)
-#toto = values
-#nc.close()
+# display verification
+print np.max(values), np.min(values), np.shape(values), np.shape(destAT)
 fig, ax = plt.subplots()
-im = ax.imshow(values)
-#patch = patches.Circle((260, 200), radius=200, transform=ax.transData)
-#im.set_clip_path(patch)
-
+im = ax.imshow(values,origin='lower')
 plt.axis('off')
 plt.show()
