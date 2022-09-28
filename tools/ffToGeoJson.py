6b8c7dc9b7846e7f463a7e45726a357a53b40af5
FILIPPI Jean-Baptiste <filippi@univ-corse.fr>
2022-09-28 08:05:17 +0200

    helper to post-process ff dumps into GeoJson

--- tools/ffToGeoJson.py
+++ tools/ffToGeoJson.py
@@ -3,20 +3,9 @@ import sys
 from pyproj import Proj, transform
 import json
 from operator import isNumberType
+    
 
-#http://dev.firecaster.valabre.fr/forefireAPI.php?command=ignition&longitude=42.348570491488999&date_simulation=2020-01-04T09%3A37%3A09Z&date_eclosion=2020-02-04T09%3A37%3A09Z&latitude=9.017890207687699&pathdate=2020-01-07_T10_47_15Z
-    
-#http://dev.firecaster.valabre.fr/forefireAPI.php?command=sequence&duree_h=4&vent_direction_degre=20&vent_vitesse_kmh=50&temperature=10&hygrometrie=10&pathdate=2020-01-07_T10_47_15Z
-    
-    
-#     $duree_h=$_REQUEST['duree_h'];
-# 11 $vent_direction_degre=$_REQUEST['vent_direction_degre'];
-# 12 $vent_vitesse_kmh=$_REQUEST['vent_vitesse_kmh'];
-# 13 $hygrometrie=$_REQUEST['hygrometrie'];
-# 14 $temperature=$_REQUEST['temperature'];
-# 15 $pathdate=$_REQUEST['pathdate'];
-
-printff = 'FireDomain[sw=(479337,4.62206e+06,0);ne=(520762,4.67757e+06,0);t=32406]\n\tFireFront[id=2;domain=0;t=32400]\n\t\tFireNode[domain=0;id=4;fdepth=100;kappa=0.0106066;loc=(511897,4.6547e+06,0);vel=(1.50339e-13,0.114793,0);t=32400;state=moving;frontId=2]\n\t\tFireNode[domain=0;id=6;fdepth=100;kappa=0.01;loc=(511977,4.65454e+06,0);vel=(0.106766,-0.0659849,0);t=32400;state=moving;frontId=2]\n\t\tFireNode[domain=0;id=8;fdepth=100;kappa=0.0124568;loc=(511817,4.65454e+06,0);vel=(-0.137972,-0.0852716,0);t=32400;state=moving;frontId=2]\n\t\tFireNode[domain=0;id=10;fdepth=100;kappa=-1.31783e-13;loc=(511843,4.65459e+06,0);vel=(-0.113095,0.0565476,0);t=32400;state=moving;frontId=2]\n\t\tFireNode[domain=0;id=12;fdepth=100;kappa=1.31783e-13;loc=(511870,4.65465e+06,0);vel=(-0.0864376,0.0432188,0);t=32400;state=moving;frontId=2]\n'
+sampleDump = 'FireDomain[sw=(479337,4.62206e+06,0);ne=(520762,4.67757e+06,0);t=32406]\n\tFireFront[id=2;domain=0;t=32400]\n\t\tFireNode[domain=0;id=4;fdepth=100;kappa=0.0106066;loc=(511897,4.6547e+06,0);vel=(1.50339e-13,0.114793,0);t=32400;state=moving;frontId=2]\n\t\tFireNode[domain=0;id=6;fdepth=100;kappa=0.01;loc=(511977,4.65454e+06,0);vel=(0.106766,-0.0659849,0);t=32400;state=moving;frontId=2]\n\t\tFireNode[domain=0;id=8;fdepth=100;kappa=0.0124568;loc=(511817,4.65454e+06,0);vel=(-0.137972,-0.0852716,0);t=32400;state=moving;frontId=2]\n\t\tFireNode[domain=0;id=10;fdepth=100;kappa=-1.31783e-13;loc=(511843,4.65459e+06,0);vel=(-0.113095,0.0565476,0);t=32400;state=moving;frontId=2]\n\t\tFireNode[domain=0;id=12;fdepth=100;kappa=1.31783e-13;loc=(511870,4.65465e+06,0);vel=(-0.0864376,0.0432188,0);t=32400;state=moving;frontId=2]\n'
 print printff
     
 class ffToGeoJson:
