a07931319265699e94bb1e9176fd6ccb9752ba6b
ludobegins <ludo.beghin@gmail.com>
2022-09-30 12:40:08 -0300

    reproject coords, create ff and output geojson

--- py3_tools/ForeFirepy3.py
+++ py3_tools/ForeFirepy3.py
@@ -1,3 +1,31 @@
-# TODO
-def create_ff():
-  return"setParameter[projection=EPSG:32632]"
+import os
+
+def create_ff(x, y):
+  ff = f'''setParameter[projection=EPSG:32632]
+setParameter[fuelsTableFile=./fuels.ff]
+setParameter[spatialIncrement=3]
+setParameter[propagationModel=Rothermel]
+setParameter[minSpeed=0.005]
+setParameter[dumpMode=json]
+setParameter[caseDirectory=.]
+setParameter[ForeFireDataDirectory=.]
+setParameter[propagationSpeedAdjustmentFactor=1]
+loadData[landscape.nc;2009-07-24T11:37:39Z]
+startFire[loc=({x},{y},0);t=0]
+step[dt=12000]
+print[./*count*-*ISOdate*.json]
+print[]
+  '''
+
+  dir_path = os.path.dirname(os.path.realpath(__file__))
+
+  c = [str(round(c, 4)).replace('.','_') for c in [x, y]]
+  filename = f'{c[0]}__{c[1]}.ff'
+  
+  output_path = dir_path + '/../examples/aullene/'
+  complete_path = output_path + filename
+
+  with open(complete_path, 'w', encoding='utf-8') as f:
+    f.write(ff)
+
+  return {'output_path': output_path, 'filename': filename}
--- py3_tools/coord_to_ff.py
+++ py3_tools/coord_to_ff.py
@@ -1,6 +1,7 @@
 import argparse
 
 from ff2geojson import *
+from ForeFirepy3 import *
 
 def main():
 	ap = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -15,12 +16,15 @@ def main():
 	args = vars(ap.parse_args())
 	lat = args.get('lat')
 	lon = args.get('lon')
-	print('Coord', lat, lon)
 
-	# reproj coords
-	# save .ff file in examples DIR ../examples/aullene
-	# os.system(forefire)
-	# conver to geojson with specific name
+	[x, y] = reproject([lon, lat], inEpsg='epsg:4326', outEpsg='epsg:32632')
+
+	output_path = create_ff(x,y)['output_path']
+	filename = create_ff(x,y)['filename']
+
+	os.system(f'cd {output_path}; ../../bin/forefire -i {filename}')
+	
+	ffjson2geojson(output_path + '0-2009-07-24T14-57-39Z.json')
 
 
 if __name__ == '__main__':
