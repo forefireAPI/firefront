80abffbe6357775d8e507716c561e4e22692d54d
antonio-leblanc <antonioleblanc@poli.ufrj.br>
2022-10-20 23:23:15 -0300

    indentation

--- src/Rothermel.cpp
+++ src/Rothermel.cpp
@@ -1,6 +1,6 @@
 /*
 
-Copyright (C) 2012 ForeFire Team, SPE, UniversitŽ de Corse.
+Copyright (C) 2012 ForeFire Team, SPE, Universitï¿½ de Corse.
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU Lesser General Public
@@ -96,8 +96,8 @@ double Rothermel::getSpeed(double* valueOf){
 	double lsd  = valueOf[sd] / 3.2808399; // conversion m -> ft
 	double le   = valueOf[e] * 3.2808399;
 	if (le==0) return 0;
-        double lSigmad = valueOf[Sigmad] * 0.2048; // conversion kg/m^2 -> lb/ft^2
-        double lDeltaH = valueOf[DeltaH] / 2326;// conversion J/kg -> BTU/lb
+	double lSigmad = valueOf[Sigmad] * 0.2048; // conversion kg/m^2 -> lb/ft^2
+	double lDeltaH = valueOf[DeltaH] / 2326;// conversion J/kg -> BTU/lb
 	double normal_wind  = valueOf[normalWind] * 196.850394 ; //conversion m/s -> ft/min
 	double localngle =  valueOf[slope];
 
@@ -123,27 +123,27 @@ double Rothermel::getSpeed(double* valueOf){
 
 	double Wn = lSigmad;
 
-        double Mratio = lMd / Mchi;
+	double Mratio = lMd / Mchi;
 
-        double Etam = 1  + Mratio * (-2.59 + Mratio * (5.11 - 3.52 * Mratio));
+	double Etam = 1  + Mratio * (-2.59 + Mratio * (5.11 - 3.52 * Mratio));
 
 	double A = 1 / (4.774 * pow(lsd, 0.1) - 7.27);
         
-        double lRhobulk = Wn / le; // Dead bulk density = Dead fuel load / Fuel height
+	double lRhobulk = Wn / le; // Dead bulk density = Dead fuel load / Fuel height
         
 	double Beta = lRhobulk / lRhod;  // Packing ratio = Bulk density / Particle density
 
-        double Betaop = 3.348 * pow(lsd, -0.8189);
+	double Betaop = 3.348 * pow(lsd, -0.8189);
 
 	double RprimeMax = pow(lsd, 1.5) * (1 / (495 + 0.0594 * pow(lsd, 1.5)));
 
-        double Rprime = RprimeMax * pow((Beta/Betaop), A) *  exp(A*(1-(Beta/Betaop))) ;
+	double Rprime = RprimeMax * pow((Beta/Betaop), A) *  exp(A*(1-(Beta/Betaop))) ;
 
-        double chi = pow(192 + 0.259*lsd, -1) * exp((0.792 + 0.681*pow(lsd, 0.5)) * (Beta+0.1));
+	double chi = pow(192 + 0.259*lsd, -1) * exp((0.792 + 0.681*pow(lsd, 0.5)) * (Beta+0.1));
 
 	double epsilon  = exp(-138 / lsd);
 
-        double Qig = 250 + 1116 * lMd; // "1,116 in Rothermel" != 1.116
+	double Qig = 250 + 1116 * lMd; // "1,116 in Rothermel" != 1.116
 
 	double C = 7.47 * exp(-0.133*pow(lsd, 0.55));
 
@@ -160,7 +160,7 @@ double Rothermel::getSpeed(double* valueOf){
 		normal_wind = Uf;
 	}
 
-        double phiV = C * pow((Beta/Betaop), -E) * pow(normal_wind,B) ;
+	double phiV = C * pow((Beta/Betaop), -E) * pow(normal_wind,B) ;
 
 	double phiP = 5.275 * pow(Beta, -0.3) * pow(tanangle, 2);
 
