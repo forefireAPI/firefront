07328e8d1dd8795fc5a3847b19ed1f064ea6e935
Thoreau Romain <romain.thoreau@cnes.fr>
2024-09-05 17:21:08 +0200

    update RothermelAndrews2018.cpp

--- src/RothermelAndrews2018.cpp
+++ src/RothermelAndrews2018.cpp
@@ -35,14 +35,17 @@ class RothermelAndrews2018: public PropagationModel {
 	static int isInitialized;
 	double windReductionFactor;
 	/*! properties needed by the model */
-	size_t slope;
-	size_t normalWind;
-	size_t Rhod;
-	size_t Md;
-	size_t sd;
-	size_t e;
-	size_t Sigmad;
-	size_t DeltaH;
+	size_t wv_;
+	size_t slope_;
+	size_t wo_;
+	size_t fd_;
+	size_t fpsa_;
+	size_t mf_;
+	size_t pp_;
+	size_t h_;
+	size_t mois_ext;
+	size_t st;
+	size_t se;
 
 	/*! result of the model */
 	double getSpeed(double*);
@@ -76,15 +79,15 @@ RothermelAndrews2018::RothermelAndrews2018(const int & mindex, DataBroker* db)
 	/* defining the properties needed for the model */
 	windReductionFactor = params->getDouble("windReductionFactor");
 
-	wv = registerProperty("normalWind_ms");
-	slope = registerProperty("slope_deg");
+	wv_ = registerProperty("normalWind_ms");
+	slope_ = registerProperty("slope_deg");
 
-	wo = registerProperty("fl1h_tac");
-	fd = registerProperty("fd_ft");
-	fpsa = registerProperty("SAVcar_ftinv");
-	mf = registerProperty("mdOnDry1h_r");
-	pp = registerProperty("fuelDens_lbft3");
-	h = registerProperty("H_BTUlb");
+	wo_ = registerProperty("fl1h_tac");
+	fd_ = registerProperty("fd_ft");
+	fpsa_ = registerProperty("SAVcar_ftinv");
+	mf_ = registerProperty("mdOnDry1h_r");
+	pp_ = registerProperty("fuelDens_lbft3");
+	h_ = registerProperty("H_BTUlb");
 	mois_ext = registerProperty("mois_ext_r");
 	st = registerProperty("st_r");
 	se = registerProperty("se_r");
@@ -120,14 +123,14 @@ double RothermelAndrews2018::getSpeed(double* valueOf){
 	double ms2ftmin = 196.85039;
 	double ftmin2ms = 0.00508;
 
-	double slope = Pi * valueOf[slope] / 180; // convert degrees to radians
-	double wv = ms2ftmin * valueOf[wv]; // convert m/s to feat/min
-	double pp = valueOf[pp];
-	double mf = valueOf[mf];
-	double fpsa = valueOf[fpsa];
-	double fd = valueOf[fd];
-	double wo = valueOf[wo];
-	double h = valueOf[h];
+	double slope = Pi * valueOf[slope_] / 180; // convert degrees to radians
+	double wv = ms2ftmin * valueOf[wv_]; // convert m/s to feat/min
+	double pp = valueOf[pp_];
+	double mf = valueOf[mf_];
+	double fpsa = valueOf[fpsa_];
+	double fd = valueOf[fd_];
+	double wo = valueOf[wo_];
+	double h = valueOf[h_];
 
 	if (wv < 0) wv = 0;
 
@@ -144,10 +147,10 @@ double RothermelAndrews2018::getSpeed(double* valueOf){
         double NS = 0.174 * pow(se, -0.19);  // Mineral damping coefficient
         double RI = T * WN * h * NM * NS;
         double PFR = pow(192.0 + 0.2595 * fpsa, -1) * exp((0.792 + 0.681 * pow(fpsa, 0.5)) * (Beta + 0.1));  // Propogating flux ratio
-        // Wind Coefficient
+        // Wind Coefficien t
         double B = 0.02526 * pow(fpsa, 0.54);
         double C = 7.47 * exp(-0.1333 * pow(fpsa, 0.55));
-        double = 0.715 * exp(-3.59 * pow(10, -4) * fpsa);
+        double E = 0.715 * exp(-3.59 * pow(10, -4) * fpsa);
         if (wv > (0.9 * RI)) wv = 0.9 * RI;
 		double WC = (C * pow(wv, B)) * pow((Beta / Beta_op), (-E));
 		double SC = 5.275*(pow(Beta, -0.3))*pow(tan_slope, 2);
@@ -158,10 +161,6 @@ double RothermelAndrews2018::getSpeed(double* valueOf){
         double numerator = (RI * PFR * (1 + WC + SC));
         double denominator = (ODBD * EHN * QIG);
         double R = numerator / denominator; // WC and SC will be zero at slope = wind = 0
-        double RT = 384.0/fpsa;
-        double HA = RI*RT;
-        // fireline intensity as described by Albini via USDA Forest Service RMRS-GTR-371. 2018
-        double FI = (384.0/fpsa)*RI*(R); // Uses Reaction Intensity in BTU / ft/ min
 
 		if(R <= 0.0) {
 			return 0;
@@ -169,6 +168,7 @@ double RothermelAndrews2018::getSpeed(double* valueOf){
 			return R * ftmin2ms;
 		}
 	}else{
-		return 0
+		return 0;
 	}
 } /* namespace libforefire */
+}
